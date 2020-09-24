3c38ea1eb7b2b24f182e1a0e5cd4cb239a20fa86
Robin Fussell <rfussell17@gmail.com>
2020-09-24 13:01:00 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// Grab the emojiContainer div and store it in a variable. Log it to the console
+
+const myEmojis = ["👨‍💻", "⛷", "🍲"]
+
+const emojiBin = document.querySelector("#emojiContainer")
+
+console.log(emojiBin)
+
+for (let i = 0; i < myEmojis.length; i++) {
+    // console.log(myEmojis[i])
+}
