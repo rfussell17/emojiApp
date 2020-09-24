c802b0cc879afa3b16abe74556373595172eb070
Robin Fussell <rfussell17@gmail.com>
2020-09-24 13:07:13 -0700

    emojis rendering on screen

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-// Grab the emojiContainer div and store it in a variable. Log it to the console
 
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
-
 const emojiBin = document.querySelector("#emojiContainer")
 
-console.log(emojiBin)
 
 for (let i = 0; i < myEmojis.length; i++) {
-    // console.log(myEmojis[i])
+  var newSpan = document.createElement("span")
+  newSpan.textContent = myEmojis[i]
+  emojiContainer.append(newSpan)
 }
