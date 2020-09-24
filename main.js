d4b662ddc572fcaee08a66005f7356c8443c75c5
Robin Fussell <rfussell17@gmail.com>
2020-09-24 16:33:13 -0700

    push and unshift functions working

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
 const emojiBin = document.querySelector("#emoji-container")
-
-
-for (let i = 0; i < myEmojis.length; i++) {
-  const emoji = document.createElement("span")
-  emoji.textContent = myEmojis[i]
-  emojiBin.append(emoji)
-}
-
-
-const addBtn = document.getElementById("push-btn")
 const inputBox = document.getElementById("emoji-input")
 
+function renderEmojis(){
+  emojiBin.innerHTML = ""
+  for (let i = 0; i < myEmojis.length; i++) {
+    const emoji = document.createElement("span")
+    emoji.textContent = myEmojis[i]
+    emojiBin.append(emoji)
+  }
+}
+
+renderEmojis()
+
+const addBtn = document.getElementById("push-btn")
 addBtn.addEventListener("click", handleAddBtn)
 
 function handleAddBtn(){
@@ -20,7 +22,18 @@ function handleAddBtn(){
   myEmojis.push(inputBox.value)
   console.log(myEmojis)
   inputBox.value = ""
-  } else {
-    inputBox.value = ""
+  renderEmojis();
   }
+}
+
+const unshiftBtn = document.getElementById("unshift-btn")
+unshiftBtn.addEventListener("click", handleUnshiftBtn)
+
+function handleUnshiftBtn(){
+  if(inputBox.value){
+    myEmojis.unshift(inputBox.value)
+    console.log(myEmojis)
+    inputBox.value = ""
+    renderEmojis();
+    }
 }
